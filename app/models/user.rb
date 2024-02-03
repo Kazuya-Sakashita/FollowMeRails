f3236fc36e18e8e31e890bc298e16c689427d1cc
Kazuya-Sakashita <kz0508@gmail.com>
2024-02-04 06:53:04 +0900

    フォロー機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,31 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # フォローしているユーザーを取得
+  has_many :active_relationships, class_name: "Relationship",
+           foreign_key: "follower_id",
+           dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+
+  # フォロワーを取得
+  has_many :passive_relationships, class_name: "Relationship",
+           foreign_key: "followed_id",
+           dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
+
+  # ユーザーをフォローする
+  def follow(other_user)
+    following << other_user unless self == other_user
+  end
+
+  # ユーザーをアンフォローする
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  # 現在のユーザーがフォローしているかどうかを確認
+  def following?(other_user)
+    following.include?(other_user)
+  end
 end
